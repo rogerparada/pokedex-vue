c2b41a316589ea14245a19711811fc8b759e53e7
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2024-04-16 19:04:08 +0200

    Update SVG and scuryty patches

---
+++ src/components/cards/EvolutionBanner.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class=""></div>
+</template>
+
+<script>
+	export default {
+		name: "EvolutionBanner",
+		props: {
+			trigger: {
+				type: String,
+				default: null,
+			},
+			evolution: {
+				type: Array,
+				default: null,
+			},
+		},
+	};
+</script>
+
+<style></style>
